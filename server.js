c138c3fbb6265fceec499f1d4bf9ffadfedebda0
SamsonGee <88246655+SamsonGee@users.noreply.github.com>
2021-11-30 17:43:44 -0600

    why is this taking me so long

--- server.js
+++ server.js
@@ -109,6 +109,7 @@ app.post('/api/candidate', ({ body }, res) => {
     });
 });
 
+// GET all parties
 app.get('/api/parties', (req, res) => {
     const sql = `SELECT * FROM parties`;
     db.query(sql, (err, rows) => {
@@ -123,6 +124,7 @@ app.get('/api/parties', (req, res) => {
     });
   });
 
+  // GET a single id
   app.get('/api/party/:id', (req, res) => {
     const sql = `SELECT * FROM parties WHERE id = ?`;
     const params = [req.params.id];
@@ -138,6 +140,7 @@ app.get('/api/parties', (req, res) => {
     });
   });
 
+  // delete a party 
   app.delete('/api/party/:id', (req, res) => {
     const sql = `DELETE FROM parties WHERE id = ?`;
     const params = [req.params.id];
@@ -158,6 +161,36 @@ app.get('/api/parties', (req, res) => {
       }
     });
   });
+
+// Update a candidate's party
+app.put('/api/candidate/:id', (req, res) => {
+    const errors = inputCheck(req.body, 'party_id');
+
+    if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+    };
+
+    const sql = `UPDATE candidates SET party_id = ? 
+               WHERE id = ?`;
+    const params = [req.body.party_id, req.params.id];
+    db.query(sql, params, (err, result) => {
+        if (err) {
+        res.status(400).json({ error: err.message });
+        // check if a record was found
+        } else if (!result.affectedRows) {
+        res.json({
+            message: 'Candidate not found'
+        });
+        } else {
+            res.json({
+                message: 'success',
+                data: req.body,
+                changes: result.affectedRows
+            });
+        }
+    });
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
