00161e4b92b9ff1b139036c25e279401f07d087f
SamsonGee <88246655+SamsonGee@users.noreply.github.com>
2021-11-30 20:48:31 -0600

    somethings wrong

--- server.js
+++ server.js
@@ -1,24 +1,12 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const mysql = require('mysql2');
 const inputCheck = require('./utils/inputCheck');
+const db = require('./db/connection');
+
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-// Connect to database
-const db = mysql.createConnection(
-    
-    {
-      host: 'localhost',
-      // Your MySQL username,
-      user: 'root',
-      // Your MySQL password
-      password: '!Madison09',
-      database: 'election'
-    },
-    console.log('Connected to the election database.')
-  );
 
 // GET all candidates '
 app.get('/api/candidates/', (req, res) => {
